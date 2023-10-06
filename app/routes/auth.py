360748f6d05771584f54964233ecc305979ae6dc
Guillermo Adell <guillermo@adell.tech>
2023-10-06 21:14:38 +0200

    Login to prod

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,16 +1,56 @@
 # auth.py
-from flask import Blueprint, render_template
+from functools import wraps
+from flask import Blueprint, render_template,request,redirect,url_for,session
+import bcrypt
+
+from app.database.seeder import fetch_all
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
+@auth_bp.route('/login',methods=['GET', 'POST'])
 def login():
-    return "AUTH"
-    return render_template('auth/login.html')
+    if request.method == 'POST':
+        fetched_users = fetch_all()   
+        print(fetched_users)    
+        fetched_emails = [x[1] for x in fetched_users]
+        fetched_passwords = [x[2] for x in fetched_users]
+
+        email = request.form['email']
+        password = request.form['password']
+
+        # Check if the username exists in the users dictionary
+        try:
+            index_user = fetched_emails.index(email)
+        except ValueError:
+            index_user = -1
+        if index_user >= 0:           
+            # Check if the entered password matches the stored hash
+            if bcrypt.checkpw(password.encode('utf-8'), fetched_passwords[index_user].encode('utf-8')):
+                session['user_id'] = fetched_users[index_user][0]
+                # Redirect to a success page or dashboard
+                return redirect(url_for('dash.dashboard'))
+            else:
+                # Display an error message if authentication fails
+                return render_template('login.html', error='Invalid credentials')
+
+        # Display an error message if the username doesn't exist
+        return render_template('login.html', error='Invalid credentials')
+
+    return render_template('login.html')
 
 @auth_bp.route("/logout")
 def logout():
-    a = 2
-    b = 3
-    z = a+b
-    return(str(z))
+    session.clear()
+    return render_template('login.html')
+
+
+def login_required(route_function):
+    @wraps(route_function)
+    def wrapper(*args,**kwargs):
+        if 'user_id' in session:
+            # User is Logged In
+            return route_function(*args,**kwargs)        
+        else:
+            # User is NOT Logged In
+            return redirect(url_for('auth.login'))
+    return wrapper
--- app/routes/dash.py
+++ app/routes/dash.py
@@ -1,20 +1,22 @@
 # dash.py
 from flask import Blueprint, render_template
 
+from app.routes.auth import login_required
 from app.graph_functions.generate_scatter import generate_scatter_plot
 
 dash_bp = Blueprint('dash', __name__)
 
 @dash_bp.route('/dashboard')
-def index():
+@login_required
+def dashboard():
     x_data = [1, 2, 3, 4, 5]
     y_data = [10, 11, 12, 13, 14]
     title = 'Sample Scatter Plot'
     plotly_plot = generate_scatter_plot(x_data, y_data, title)
-    print(plotly_plot)
     return render_template('dashboard.html', plotly_plot=plotly_plot)
     
 @dash_bp.route('/prueba')
+@login_required
 def index_prueba():
     return render_template('template.html')
     

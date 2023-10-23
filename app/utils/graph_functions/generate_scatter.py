08b7565e1d006f2e484075b9d7fc8d0b4da5fa86
ariadnaruiz25 <ariadnaruiz25@gmail.com>
2023-10-23 11:18:50 +0200

    v3 dashboard

--- app/utils/graph_functions/generate_scatter.py
+++ app/utils/graph_functions/generate_scatter.py
@@ -3,8 +3,7 @@ from flask import jsonify
 def generate_scatter_plot(x_data, y_data, title):
     fig = px.scatter(x=x_data, y=y_data, title=title)   
 
-    #fig.update_layout(width=850,height=390)
-    fig.update_layout({'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    fig.update_layout({'paper_bgcolor': 'rgba(0, 0, 0, 0)'},margin=dict(l=20, r=20, t=55, b=20))
     serialized_fig = fig.to_json()
 
     return serialized_fig

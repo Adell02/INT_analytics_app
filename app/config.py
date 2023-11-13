a832f1111ee944b06a305c3c6a01805e63e39cd9
Guillermo Adell <guillermo@adell.tech>
2023-11-13 17:04:50 +0100

    Config Change in PROD - RAY DB

--- app/config.py
+++ app/config.py
@@ -13,6 +13,11 @@ class Config:
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
 
+    MYSQL_HOST_RAY = os.getenv("MYSQL_HOST_RAY")
+    MYSQL_USER_RAY = os.getenv("MYSQL_USER_RAY")
+    MYSQL_PASSWORD_RAY = os.getenv("MYSQL_PASSWORD_RAY")
+    MYSQL_DB_RAY = os.getenv("MYSQL_DB_RAY")
+
     SESSION_TYPE= os.getenv("SESSION_TYPE")
     SESSION_PERMANENT= False
     SESSION_USE_SIGNER=True
